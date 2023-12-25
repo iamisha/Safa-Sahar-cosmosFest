5459e1bbcb9064b638f926c3d29d0727f67fcfa1
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-12-26 05:33:31 +0545

    git add admin route

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import { Card} from "@/components/card";
+
+export default function Home({}) {
+    return (
+      <>
+        <div className="flex flex-row space-x-4 items-center justify-center min-h-screen py-2 bg-white">
+           <span className="text-black"> This is admin Stuff</span>
+        </div>
+      </>
+    );
+  }
